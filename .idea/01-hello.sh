7e9e5d8b076d9bebcc43f9d8657073586518bedf
Rajakumar <rajakumarbattula@gmail.com>
2021-02-11 14:03:36 +0530

    updated...

--- .idea/01-hello.sh
+++ .idea/01-hello.sh
@@ -3,5 +3,5 @@
 echo -e "hello.. \tworld \nwelcome to Devops"
 a=10
 echo $a
-echo -e "\e[1;33mI\e[0m" "\e[4;34mLove\e[0m" "\e[14;36mIndia\e[0m"
+echo -e "\e[1;33mI\e[0m" "\e[4;34mLove\e[0m" "\e[1;36mIndia\e[0m"
 echo bye
