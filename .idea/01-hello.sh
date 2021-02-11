875d27aeac586cf35a4aa5206bae72e574e28fcd
Rajakumar <rajakumarbattula@gmail.com>
2021-02-11 14:23:23 +0530

    updated......

--- .idea/01-hello.sh
+++ .idea/01-hello.sh
@@ -2,6 +2,6 @@
 #this is my first shell scripting
 echo -e "hello.. \tworld \nwelcome to Devops"
 a=10
-echo $a
+echo the number of apples in the basket are ${a}no
 echo -e "\e[1;4;33mI\e[0m" "\e[4;34mLove\e[0m" "\e[1;4;36mIndia\e[0m"
 echo bye
