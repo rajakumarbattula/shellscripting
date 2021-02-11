62b8e7779505d7fd3a8eb926f160a566c94860fe
Rajakumar <rajakumarbattula@gmail.com>
2021-02-11 15:38:13 +0530

    updated...

--- .idea/01-hello.sh
+++ .idea/01-hello.sh
@@ -4,6 +4,6 @@ echo -e "hello.. \tworld \nwelcome to Devops"
 a=10
 echo the number of apples in the basket are ${a}no
 echo -e "\e[1;4;33mI\e[0m" "\e[4;34mLove\e[0m" "\e[1;4;36mIndia\e[0m"
-abc=(10,20,30)
-echo the element in an array is ${abc[1]}
+d=(10 20 30)
+echo the element in an array is ${d[0]}
 echo the value of A is : $A
