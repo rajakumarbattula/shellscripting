9e57402829510b294c11768af38423ee9349e747
Rajakumar <rajakumarbattula@gmail.com>
2021-02-11 15:25:48 +0530

    updated......

--- .idea/01-hello.sh
+++ .idea/01-hello.sh
@@ -4,4 +4,5 @@ echo -e "hello.. \tworld \nwelcome to Devops"
 a=10
 echo the number of apples in the basket are ${a}no
 echo -e "\e[1;4;33mI\e[0m" "\e[4;34mLove\e[0m" "\e[1;4;36mIndia\e[0m"
-echo bye
+abc=(10,20,30)
+echo the element in an array is ${abc[0]}
