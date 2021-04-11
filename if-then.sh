77cfd3420e14ced56b4d4bef26ce9233c5f9bafe
Rajakumar <rajakumarbattula@gmail.com>
2021-04-11 22:18:55 +0530

    up

--- if-then.sh
+++ if-then.sh
@@ -2,11 +2,10 @@
 
 
 a=100
-# shellcheck disable=SC1072
-# shellcheck disable=SC1009
-if [ $a == 100]
+
+if [ "$a" -eq 100]
 then
-  echo " Count is 100"
+  echo "Count is 100"
 else
-  echo " Count is not 100"
+  echo "Count is not 100"
 fi
